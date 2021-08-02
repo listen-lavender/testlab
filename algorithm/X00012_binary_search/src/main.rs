3be5b700dcbd6754bd0b161c37038582c2096af8
haokuan <haokuan@yunzujia.com>
2021-08-02 14:40:31 +0800

    fix bug

--- algorithm/X00012_binary_search/src/main.rs
+++ algorithm/X00012_binary_search/src/main.rs
@@ -8,7 +8,6 @@ fn binary_search(nums:&[u32], target: u32) -> i32 {
     let mut ind:i32 = -1;
     loop {
         if left > right {
-            ind = -1;
             break;
         }
         // thread::sleep_ms(1000);
@@ -45,6 +44,87 @@ fn recursive_binary_search(nums:&[u32], left:usize, right:usize, target: u32) ->
     }
 }
 
+fn left_bound(nums:&[u32], target:u32) -> i32 {
+    if nums.len() == 0 {
+        -1
+    } else {
+        // let mut left:usize = 0;
+        // let mut right = nums.len();
+        // let mut ind:i32 = -1;
+        // loop {
+        //     if left >= right {
+        //         ind = left as i32;
+        //         break;
+        //     }
+        //     // thread::sleep_ms(1000);
+        //     let mid = left + (right - left)/2;
+        //     if nums[mid] < target {
+        //         left = mid + 1;
+        //     } else if nums[mid] > target {
+        //         right = mid;
+        //     } else {
+        //         right = mid;
+        //     }
+        // }
+        // if left >= nums.len() || nums[left] != target {
+        //     ind = -1;
+        // }
+        // ind
+        let mut left:usize = 0;
+        let mut right = nums.len() - 1;
+        let mut ind:i32 = -1;
+        loop {
+            if left > right {
+                ind = left as i32;
+                break;
+            }
+            // thread::sleep_ms(1000);
+            let mid = left + (right - left)/2;
+            if nums[mid] < target {
+                left = mid + 1;
+            } else if nums[mid] > target {
+                right = mid - 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if left >= nums.len() || nums[left] != target {
+            ind = -1;
+        }
+        ind
+    }
+}
+
+fn right_bound(nums:&[u32], target:u32) -> i32 {
+    if nums.len() == 0 {
+        -1
+    } else {
+        let mut left:usize = 0;
+        let mut right = nums.len() - 1;
+        let mut ind:i32 = -1;
+        loop {
+            if left > right {
+                left = left - 1;
+                ind = left as i32;
+                break;
+            }
+            // thread::sleep_ms(1000);
+            let mid = left + (right - left)/2;
+            if nums[mid] < target {
+                left = mid + 1;
+            } else if nums[mid] > target {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        if left >= nums.len() || nums[left] != target {
+            ind = -1;
+        }
+        ind
+    }
+}
+
 fn main() {
     let nums = &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let target = 5;
@@ -59,4 +139,15 @@ fn main() {
     println!("recursive_binary_search {:?} {}: {}", nums, target, recursive_binary_search(nums, 0, nums.len(), target));
     let target = 10;
     println!("recursive_binary_search {:?} {}: {}", nums, target, recursive_binary_search(nums, 0, nums.len(), target));
+    let nums = &[1, 2, 3, 5, 5, 5, 5, 8, 9, 10];
+    let target = 5;
+    println!("bound {:?} {}: left {}, right {}", nums, target, left_bound(nums, target), right_bound(nums, target));
+    let nums = &[1, 2, 3, 4, 5, 7, 8, 9, 10, 11];
+    let target = 6;
+    println!("bound {:?} {}: left {} right {}", nums, target, left_bound(nums, target), right_bound(nums, target));
+    let target = 5;
+    println!("bound {:?} {}: left {} right {}", nums, target, left_bound(nums, target), right_bound(nums, target));
+    let target = 7;
+    println!("===");
+    println!("bound {:?} {}: left {} right {}", nums, target, left_bound(nums, target), right_bound(nums, target));
 }
