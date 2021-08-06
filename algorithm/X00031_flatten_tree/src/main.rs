f461f5b6ec65bb2dd9843c50459ff4458b227ba8
haokuan <haokuan@yunzujia.com>
2021-08-06 17:32:35 +0800

    展开二叉树

--- algorithm/X00031_flatten_tree/src/main.rs
+++ algorithm/X00031_flatten_tree/src/main.rs
@@ -21,67 +21,118 @@ impl TreeNode {
     }
 }
 
-fn flatten_tree(node: Option<Rc<RefCell<TreeNode>>>) {
-    match node {
-        Some(raw_node) => {
-            println!("{} ====", &raw_node.borrow().value);
-            match &raw_node.borrow().left {
-                Some(left_rc_node) => {
-                    flatten_tree(Some(left_rc_node.clone()));
+// fn flatten_tree(node: Option<Rc<RefCell<TreeNode>>>) {
+//     match node {
+//         Some(raw_node) => {
+//             println!("{} ====", &raw_node.borrow().value);
+//             match &raw_node.borrow().left {
+//                 Some(left_rc_node) => {
+//                     flatten_tree(Some(left_rc_node.clone()));
 
-                    match &raw_node.borrow().right {
-                        Some(right_rc_node) => {
-                            flatten_tree(Some(right_rc_node.clone()));
-                            println!("{} ====after", &raw_node.borrow().value);
+//                     match &raw_node.borrow().right {
+//                         Some(right_rc_node) => {
+//                             flatten_tree(Some(right_rc_node.clone()));
+//                             println!("{} ====after", &raw_node.borrow().value);
 
-                            raw_node.borrow_mut().left = None;
-                            raw_node.borrow_mut().right = Some(left_rc_node.clone());
+//                             raw_node.borrow_mut().left = None;
+//                             raw_node.borrow_mut().right = Some(left_rc_node.clone());
 
-                            // let mut left_rc_node = left_rc_node.clone();
-                            // let mut shadow_rc_node:Rc<RefCell<TreeNode>>;
-                            // while let Some(rc_node) = &left_rc_node.borrow().right {
-                            //     shadow_rc_node = rc_node.clone();
-                            //     left_rc_node = shadow_rc_node;
-                            // }
+//                             // let mut left_rc_node = left_rc_node.clone();
+//                             // let mut shadow_rc_node:Rc<RefCell<TreeNode>>;
+//                             // while let Some(rc_node) = &left_rc_node.borrow().right {
+//                             //     shadow_rc_node = rc_node.clone();
+//                             //     left_rc_node = shadow_rc_node;
+//                             // }
 
-                            let mut left_rc_node = left_rc_node.clone();
-                            let mut left_node = Some(left_rc_node.clone());
-                            let mut shadow_rc_node:Rc<RefCell<TreeNode>>;
-                            while let Some(rc_node) = &left_node {
-                                shadow_rc_node = rc_node.clone();
-                                match &shadow_rc_node.borrow().right {
-                                    Some(right_rc_node) => {
-                                        left_rc_node = right_rc_node.clone();
-                                        left_node = Some(left_rc_node.clone());
-                                    }
-                                    None => {
-                                        left_node = None;
-                                    }
-                                }
-                            }
-                            left_rc_node.borrow_mut().right = Some(right_rc_node.clone());
-                        }
-                        None => {
-                            raw_node.borrow_mut().left = None;
-                            raw_node.borrow_mut().right = Some(left_rc_node.clone());
-                        }
-                    }
-                }
-                None => {
-                    match &raw_node.borrow().right {
-                        Some(right_rc_node) => {
-                            flatten_tree(Some(right_rc_node.clone()));
-                        }
-                        None => {
-                            println!("{} ====before", &raw_node.borrow().value);
-                        }
-                    }
-                }
+//                             let mut left_rc_node = left_rc_node.clone();
+//                             let mut left_node = Some(left_rc_node.clone());
+//                             let mut shadow_rc_node:Rc<RefCell<TreeNode>>;
+//                             while let Some(rc_node) = &left_node {
+//                                 shadow_rc_node = rc_node.clone();
+//                                 match &shadow_rc_node.borrow().right {
+//                                     Some(right_rc_node) => {
+//                                         left_rc_node = right_rc_node.clone();
+//                                         left_node = Some(left_rc_node.clone());
+//                                     }
+//                                     None => {
+//                                         left_node = None;
+//                                     }
+//                                 }
+//                             }
+//                             left_rc_node.borrow_mut().right = Some(right_rc_node.clone());
+//                         }
+//                         None => {
+//                             raw_node.borrow_mut().left = None;
+//                             raw_node.borrow_mut().right = Some(left_rc_node.clone());
+//                         }
+//                     }
+//                 }
+//                 None => {
+//                     match &raw_node.borrow().right {
+//                         Some(right_rc_node) => {
+//                             flatten_tree(Some(right_rc_node.clone()));
+//                         }
+//                         None => {
+//                             println!("{} ====before", &raw_node.borrow().value);
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//         None => {
+//         }
+//     }
+// }
+
+fn flatten_tree(node: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+    if node.is_none() {
+        return None
+    }
+    
+    let node = node.unwrap();
+    // println!("{} ====", &node.borrow().value);
+    let left_node = node.borrow_mut().left.take();
+    let right_node = node.borrow_mut().right.take();
+    let left_node = flatten_tree(left_node);
+    let right_node = flatten_tree(right_node);
+
+    if left_node.is_some() && right_node.is_some() {
+        let mut left_rc_node = left_node.unwrap();
+        let mut left_node = Some(left_rc_node.clone());
+        let mut right_rc_node = right_node.unwrap();
+        let mut right_node = Some(right_rc_node.clone());
+        node.borrow_mut().right = Some(left_rc_node.clone());
+
+        while let Some(rc_node) = left_node {
+            left_rc_node = rc_node.clone();
+            left_node = left_rc_node.borrow_mut().right.take();
+            if left_node.is_some() {
+                let rc_node = left_node.unwrap();
+                left_node = Some(rc_node.clone());
+                left_rc_node.borrow_mut().right = Some(rc_node.clone());
             }
         }
-        None => {
-        }
+
+        node.borrow_mut().left = None;
+        left_rc_node.borrow_mut().right = right_node;
+        // println!("{} ---both {} {}", &node.borrow().value, left_rc_node.borrow().value, right_rc_node.borrow().value);
+        // test_flatten_tree(&node.borrow().value.to_string(), node.clone());
+        // test_flatten_tree(&left_rc_node.borrow().value.to_string(), left_rc_node.clone());
+        // test_flatten_tree(&right_rc_node.borrow().value.to_string(), right_rc_node.clone());
+    } else if left_node.is_some() {
+        let mut left_rc_node = left_node.unwrap();
+        let mut left_node = Some(left_rc_node.clone());
+        node.borrow_mut().left = None;
+        node.borrow_mut().right = left_node;
+        // println!("{} ---side", &node.borrow().value);
+        // test_flatten_tree(&node.borrow().value.to_string(), node.clone());
+        // test_flatten_tree(&left_rc_node.borrow().value.to_string(), left_rc_node.clone());
+    } else {
+        // println!("{} ---", &node.borrow().value);
+        // test_flatten_tree(&node.borrow().value.to_string(), node.clone());
     }
+
+    Some(node)
 }
 
 fn init_tree() -> Rc<RefCell<TreeNode>> {
@@ -135,28 +186,14 @@ fn init_tree() -> Rc<RefCell<TreeNode>> {
 }
 
 fn test_flatten_tree(mark: &str, tree: Rc<RefCell<TreeNode>>){
-    println!("=====start max_path {} tree", mark);
+    println!("=====start {} tree", mark);
     let root = tree.clone();
     flatten_tree(Some(root));
-    let mut tree = Some(tree);
-    let mut shadow_rc_node:Rc<RefCell<TreeNode>>;
-    match tree {
-        Some(_) => {
-            // tree = &Some(rc_node.clone());
-            while let Some(rc_node) = &tree {
-                print!("{} ", rc_node.borrow().value);
-                shadow_rc_node = rc_node.clone();
-                match &shadow_rc_node.borrow().right {
-                    Some(right_rc_node) => {
-                        tree = Some(right_rc_node.clone());
-                    }
-                    None => {
-                        tree = None;
-                    }
-                }
-            }
-        }
-        None => {}
+    let mut right_rc_node = tree.clone();
+    let mut right_node = Some(right_rc_node.clone());
+    while let Some(rc_node) = right_node {
+        print!("{} ", rc_node.borrow().value);
+        right_node = rc_node.borrow_mut().right.take();
     }
     println!("");
     println!("=====end {} tree", mark);
