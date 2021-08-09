697e27f5a3a3fd62f67a6be3964d3c51a24e5254
haokuan <haokuan@yunzujia.com>
2021-08-09 15:10:54 +0800

    合法的二叉搜索树

---
+++ algorithm/X00036_valid_bst/src/main.rs
@@ -0,0 +1,204 @@
+use std::rc::Rc;
+use std::mem::swap;
+use std::mem::size_of;
+use std::cmp::max;
+use std::cell::RefCell;
+use std::cell::Cell;
+use std::collections::HashMap;
+
+struct TreeNode {
+    value: u32,
+    sum: u32,
+    left: Option<Rc<RefCell<TreeNode>>>,
+    right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    fn new(value: u32) -> Rc<RefCell<TreeNode>> {
+        Rc::new(RefCell::new(TreeNode {
+            value,
+            sum: 0,
+            left: None,
+            right: None,
+        }))
+    }
+
+    fn left_node(&mut self) -> Option<Rc<RefCell<TreeNode>>> {
+        let left_node = self.left.take();
+        if left_node.is_some() {
+            let left_node = left_node.unwrap();
+            self.left = Some(left_node.clone());
+            Some(left_node)
+        } else {
+            None
+        }
+    }
+
+    fn right_node(&mut self) -> Option<Rc<RefCell<TreeNode>>> {
+        let right_node = self.right.take();
+        if right_node.is_some() {
+            let right_node = right_node.unwrap();
+            self.right = Some(right_node.clone());
+            Some(right_node)
+        } else {
+            None
+        }
+    }
+}
+
+fn valid_bst(root: &Option<Rc<RefCell<TreeNode>>>, min: &Option<Rc<RefCell<TreeNode>>>, max: &Option<Rc<RefCell<TreeNode>>>) -> bool {
+    match root {
+        Some(raw_root_node) => {
+            let mut valid = true;
+            valid = valid && valid_bst(&raw_root_node.borrow().left, &None, &Some(raw_root_node.clone()));
+            if valid {
+                match min {
+                    Some(raw_min_node) => {
+                        valid = valid && raw_min_node.borrow().value <= raw_root_node.borrow().value;
+                    }
+                    None => {}
+                }
+            }
+            if valid {
+                match max {
+                    Some(raw_max_node) => {
+                        valid = valid && raw_max_node.borrow().value >= raw_root_node.borrow().value;
+                    }
+                    None => {}
+                }
+            }
+            if valid {
+                valid = valid && valid_bst(&raw_root_node.borrow().right, &Some(raw_root_node.clone()), &None);
+            }
+            valid
+        }
+        None => {
+            true
+        }
+    }
+}
+
+fn traverse(node: &Option<Rc<RefCell<TreeNode>>>) {
+    match node {
+        Some(raw_node) => {
+            traverse(&raw_node.borrow().left);
+            let value = raw_node.borrow().value;
+            let sum = raw_node.borrow().sum;
+            println!("{} {}", sum, value);
+            traverse(&raw_node.borrow().right);
+        }
+        None => {
+        }
+    }
+}
+
+fn init_valid_bst_tree() -> Option<Rc<RefCell<TreeNode>>> {
+    let root = TreeNode::new(4);
+    
+    {
+        let left_node = TreeNode::new(2);
+        left_node.borrow_mut().left = None;
+        left_node.borrow_mut().right = None;
+
+        let right_node = TreeNode::new(6);
+        right_node.borrow_mut().left = None;
+        right_node.borrow_mut().right = None;
+
+        let mut node = root.borrow_mut();
+        node.left = Some(left_node);
+        node.right = Some(right_node);
+    }
+
+    {
+        let left_node = TreeNode::new(1);
+        left_node.borrow_mut().left = None;
+        left_node.borrow_mut().right = None;
+
+        let right_node = TreeNode::new(3);
+        right_node.borrow_mut().left = None;
+        right_node.borrow_mut().right = None;
+
+        let node = root.borrow();
+        let mut node = node.left.as_ref().unwrap().borrow_mut();
+        node.left = Some(left_node);
+        node.right = Some(right_node);
+    }
+
+    {
+        let left_node = TreeNode::new(5);
+        left_node.borrow_mut().left = None;
+        left_node.borrow_mut().right = None;
+
+        let right_node = TreeNode::new(7);
+        right_node.borrow_mut().left = None;
+        right_node.borrow_mut().right = None;
+
+        let node = root.borrow();
+        let mut node = node.right.as_ref().unwrap().borrow_mut();
+        node.left = Some(left_node);
+        node.right = Some(right_node);
+    }
+
+    return Some(root)
+}
+
+fn init_invalid_bst_tree() -> Option<Rc<RefCell<TreeNode>>> {
+    let root = TreeNode::new(4);
+    
+    {
+        let left_node = TreeNode::new(2);
+        left_node.borrow_mut().left = None;
+        left_node.borrow_mut().right = None;
+
+        let right_node = TreeNode::new(6);
+        right_node.borrow_mut().left = None;
+        right_node.borrow_mut().right = None;
+
+        let mut node = root.borrow_mut();
+        node.left = Some(left_node);
+        node.right = Some(right_node);
+    }
+
+    {
+        let left_node = TreeNode::new(1);
+        left_node.borrow_mut().left = None;
+        left_node.borrow_mut().right = None;
+
+        let right_node = TreeNode::new(3);
+        right_node.borrow_mut().left = None;
+        right_node.borrow_mut().right = None;
+
+        let node = root.borrow();
+        let mut node = node.left.as_ref().unwrap().borrow_mut();
+        node.left = Some(left_node);
+        node.right = Some(right_node);
+    }
+
+    {
+        let left_node = TreeNode::new(7);
+        left_node.borrow_mut().left = None;
+        left_node.borrow_mut().right = None;
+
+        let right_node = TreeNode::new(5);
+        right_node.borrow_mut().left = None;
+        right_node.borrow_mut().right = None;
+
+        let node = root.borrow();
+        let mut node = node.right.as_ref().unwrap().borrow_mut();
+        node.left = Some(left_node);
+        node.right = Some(right_node);
+    }
+
+    return Some(root)
+}
+
+fn test_bst(mark: &str, tree: Option<Rc<RefCell<TreeNode>>>){
+    println!("=====start {} tree", mark);
+    println!("{}", valid_bst(&tree, &None, &None));
+    println!("=====end {} tree", mark);
+}
+
+fn main() {
+    test_bst("valid bst", init_valid_bst_tree());
+    test_bst("invalid bst", init_invalid_bst_tree());
+}
